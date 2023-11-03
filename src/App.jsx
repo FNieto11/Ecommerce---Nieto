68c27e48ea243313826f93683886758382e8ba71
FNieto11 <facundo.nieto@live.com.ar>
2023-11-03 01:44:25 -0300

    1er Pre-Entrega

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import './App.css';
 import NavBar from "./components/NavBar/NavBar";
 import ItemListConteiner from './components/ItemListConteiner/ItemListConteiner';
 
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,18 +1,33 @@
 import CartWidget from "../CartWidget/CartWidget"
 
 const NavBar = () => {
-    return(
+    return (
         <>
-        <nav>
-            <h3>Ecommerce</h3>
-            <div>
-                <button>Tortas</button>
-                <button>Macarons</button>
-                <button>Cookies y Alfajores</button>
-                <button>Petits Fours</button>
-            </div>
-        </nav>
-        <CartWidget/>
+            <nav class="navbar navbar-expand-lg">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="./index.html"><img class="logo" src="./src/assets/images/logos/chez-fabienne-high-resolution-logo-color-on-transparent-background.png" alt="Chez Fabienne"/></a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link" href="./index.html">Inicio</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="./pages/nosotros.html">Nosotros</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="./pages/nuestrosProductos.html">Nuestros Productos</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="./pages/contacto.html">Contacto</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav >
+            <CartWidget/>
         </>
     )
 }
