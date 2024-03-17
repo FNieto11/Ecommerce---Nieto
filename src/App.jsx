40b4a64ddc55dd47b43b5dddf8ebbb164ca7e451
FNieto11 <facundo.nieto@live.com.ar>
2024-03-17 11:50:43 -0300

    Creación Proyecto

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Banner from "./components/Banner"
+import './App.css'
+
+function App() {
+  
+  return (
+    <div>
+      <Banner/>
+      <h1>Hola Mundo!!</h1>
+      <p>Hola Facundo</p>
+      <Banner/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Banner.jsx
@@ -0,0 +1,7 @@
+const Banner = () => {
+    return(
+        <img src="/img/online_shopping_1.avif" style={{width: "75%"}}/>
+    )
+}
+
+export default Banner
