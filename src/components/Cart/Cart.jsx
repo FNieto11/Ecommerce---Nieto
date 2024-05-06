04e4f7bd4cc3bf9e89389a3a9de80e0e47f9aeea
FNieto11 <facundo.nieto@live.com.ar>
2024-05-06 00:12:58 -0300

    Corrección Estilos - Firebase Pendiente

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -24,15 +24,15 @@ const Cart = () => {
                         <img className="image-product-cart" src={product.image}/>
                         <p className="info-product-cart">{product.name}</p>
                         <p className="info-product-cart">Cantidad: {product.quantity}</p>
-                        <p className="info-product-cart">Precio Unit.: {product.price}</p>
-                        <p className="info-product-cart">Total: {product.quantity * product.price}</p>
+                        <p className="info-product-cart">Precio Unit.: $ {product.price}</p>
+                        <p className="info-product-cart">Total: $ {product.quantity * product.price}</p>
 
                         <button className="botonArtCarrito" onClick={()=>deleteProductById(product.id)}>Eliminar Producto</button>
                 </div>
                 ))}
             </div>
             <div className="controls-cart">
-                <h2>Precio Total: {totalPrice()}</h2>
+                <h2>Precio Total: $ {totalPrice()}</h2>
                 <button className="button-cart" onClick={clearCart}>Vaciar Carrito</button>
             </div>
         </div>
